f9ad9e6f80c5c0137a31f873bc546af2e4c11fa8
Maxim Galushka <mgalushka@gmail.com>
2014-01-14 12:35:21 +0200

    initial version

---
+++ HappyBirthday_4.ino
@@ -0,0 +1,15 @@
+/**
+ * My son Kostya has his 4th birthday on 17-Jan.
+ *
+ * I decided to make a small present to him.
+ * 
+ * To make electronic Pi with 4 diodes lightning in some different patterns
+ */
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-happy-birthdeay-4
-=================
-
-/**  * My son Kostya has his 4th birthday on 17-Jan.  *  * I decided to make a small present to him.  *   * To make electronic Pi with 4 diodes lightning in some different patterns  */
