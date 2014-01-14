8f08cb9806aa4d9e2bba20710a9b4cf0020a2df5
Maxim Galushka <mgalushka@gmail.com>
2014-01-14 12:48:43 +0200

    input/output

--- HappyBirthday_4.ino
+++ HappyBirthday_4.ino
@@ -6,10 +6,21 @@
  * To make electronic Pi with 4 diodes lightning in some different patterns
  */
 
+// pins to control candles
+static int ledPins[] = {
+  2, 3, 4, 5};
+
+static int buttonPin = 6;
+int animationStatus = 0;
+
 void setup(){
-  
+  for(int i = 0; i <= 3; i++){
+    pinMode(ledPins[i], OUTPUT);  // declare LEDs as output
+  }
+  pinMode(buttonPin, INPUT);      // declare pushbutton as input
 }
 
 void loop(){
-  
+
 }
+
